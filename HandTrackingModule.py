5758ffc4b241ec8305157904d738991573faba97
Himanshu Bhoir <himanshubhoir25@gmail.com>
2022-08-27 08:29:05 +0530

    signlang removed

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -30,7 +30,7 @@ class handDetector():
                 # print(id, cx, cy)
                 lmList.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
         return lmList
 def main():
     pTime = 0
